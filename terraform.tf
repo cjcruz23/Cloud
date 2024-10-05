334584960cd7d1ae941f6beadb56e378d363d0f1
cjcruz23 <cejo17@hotmail.com>
2024-10-05 11:02:13 -0500

    Carga Proyecto

--- terraform.tf
+++ terraform.tf
@@ -129,8 +129,31 @@ resource "aws_instance" "CarlosJ" {
   instance_type = "t3.micro"
   key_name = aws_key_pair.cloud2.key_name
 
+  subnet_id = aws_subnet.public_subnet_1.id
+
+  # Seguridad de la instancia (acceso solo por SSH - puerto 22)
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+  associate_public_ip_address = true
+
   tags = {
     Name = "CarlosJ"
+  }
+}
+
+# Crear una instancia EC2 asociada a la subred de la VPC
+resource "aws_instance" "CarlosJ2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name = aws_key_pair.cloud2.key_name
+
+  subnet_id = aws_subnet.public_subnet_2.id
+
+  # Seguridad de la instancia (acceso solo por SSH - puerto 22)
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "CarlosJ2"
   }
 }
 
